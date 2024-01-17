9f1c01e84bd94338d668436eea3f07b0d6e66947
MaraisaFerreira <ferreira.maraisacristina@gmail.com>
2024-01-17 08:52:29 -0300

    get db objects

--- pet/views/pet_views.py
+++ pet/views/pet_views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
+from pet.models import Pet
 
 
 def home(request):
+    pets_list = Pet.objects.all()
+    print(pets_list)
+
     return render(
         request,
-        'pet/home.html'
+        'pet/home.html',
+        {
+            'doc_title': 'Pets',
+            'pets': pets_list
+        }
     )
