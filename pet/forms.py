aa1d24181505c4d937048937bce98c3f6efe9695
MaraisaFerreira <ferreira.maraisacristina@gmail.com>
2024-01-18 17:10:07 -0300

    pet form

--- pet/forms.py
+++ pet/forms.py
@@ -2,6 +2,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
 from django import forms
+from pet import models
 
 from pet.models import Pet, PetImages
 
