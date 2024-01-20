7ca3119f3fca106ded6604d9581917b16a1e1fc6
MaraisaFerreira <ferreira.maraisacristina@gmail.com>
2024-01-20 12:02:51 -0300

    register user

--- pet/views/user_views.py
+++ pet/views/user_views.py
@@ -1,9 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from pet.forms import RegisterUser
+from django.contrib import messages
 
 
 def register_user(request):
     form = RegisterUser()
+
+    if request.method == 'POST':
+        form = RegisterUser(request.POST)
+
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Usuário Cadastrado!')
+
+            return redirect('pets:home')
 
     return render(
         request,
